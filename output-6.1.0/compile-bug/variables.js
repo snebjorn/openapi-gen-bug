ee78e17b3e1e8369eed090b4ab4694d3b34ee84d
Eskild Diderichsen <esd@it-minds.dk>
2022-09-15 16:16:42 +0200

    added compile-bug

---
+++ output-6.1.0/compile-bug/variables.js
@@ -0,0 +1,11 @@
+"use strict";
+exports.__esModule = true;
+exports.COLLECTION_FORMATS = exports.BASE_PATH = void 0;
+var core_1 = require("@angular/core");
+exports.BASE_PATH = new core_1.InjectionToken('basePath');
+exports.COLLECTION_FORMATS = {
+    'csv': ',',
+    'tsv': '   ',
+    'ssv': ' ',
+    'pipes': '|'
+};
